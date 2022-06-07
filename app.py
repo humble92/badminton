2fcbc9eb28f0fba05afca114c42bb78ec048d42d
Daniel Hwang <humble92@gmail.com>
2022-06-07 14:06:02 -0700

    ADD: basic UI for searching & excel download on the flask

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, send_file
 
 app = Flask(__name__)
 app.secret_key = 'dafk;ladsjghdsagljk'
+PROGRAM = "Volleyball"
 
 @app.route('/')
 def home():
-    return 'project home page for basic UI'
+    # for future usage, we build features at /search endpoint
+    # instead of /
+    return redirect(url_for('search'))
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        return render_template('search.html', program=PROGRAM)
+
+    from scraper import scrape_item
+    program = request.form['program']
+    results = scrape_item(program=program)
+    return render_template('search.html', program=program, results=results, total=len(results))
+
+@app.route("/export")
+def export():
+  try:
+    program = request.args.get('program')
+    return send_file(f'programs_data/csv/{program}.csv')
+  except:
+    # just in case: after searching, csv file should exist
+    return redirect("/")
--- manage_csv.py
+++ manage_csv.py
@@ -1,10 +1,10 @@
 import csv
 
 def save_csv(filename, json):
-    filepath = f'programs_data/csv/{filename}'
+    filepath = f'programs_data/csv/{filename}.csv'
     file = open(filepath, mode="w")
     writer = csv.writer(file)
-    writer.writerow(["location", "day", "timeslot", "age"])
+    writer.writerow(["location", "day", "timeslot", "age", "url"])
     for o in json:
         writer.writerow(o.values()) #list(o.values())
     return
